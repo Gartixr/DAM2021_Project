a495604edc93dc49d607e0fba26ee141d1473542
Adrián Jurado Villegas <adrianjjvv@gmail.com>
2021-05-10 19:53:24 +0200

    Shoot

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 20f;
+    public int damage;
+    public Rigidbody2D rb;
+    public GameObject impactEffect;
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            Debug.Log("hit");
+            enemy.TakeDamage(damage);
+        }
+        GameObject effect = Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effect, 1f);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,34 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb2d;
     public PlayerStats playerStats;
-    Vector2 movement;
-
+    public Vector2 movement;
+    private Boolean m_facingRight;
+    
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        // if (movement.x > 0 && m_facingRight)
+        // {
+        //     Flip();
+        // }
+        // else if(movement.x < 0 && !m_facingRight)
+        // {
+        //     Flip();
+        // } 
+
     }
 
     void FixedUpdate()
     {
         rb2d.MovePosition(rb2d.position + movement * playerStats.GetSpeed() * Time.fixedDeltaTime);
+    }
+
+    private void Flip()
+    {
+        m_facingRight = !m_facingRight;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
