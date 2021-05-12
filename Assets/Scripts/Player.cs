6533bd23134ff96b05c44457d53ec2998734ca2a
Adrián Jurado Villegas <adrianjjvv@gmail.com>
2021-05-12 20:56:57 +0200

    knockback enemy

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerStats playerStats;
+    public int fallBoundary = -20;
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= fallBoundary)
+        {
+            DamagePlayer(99999999);
+        }
+    }
+
+    public void DamagePlayer(int damage)
+    {
+        playerStats.curHealth -= damage;
+        if (playerStats.curHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+}
