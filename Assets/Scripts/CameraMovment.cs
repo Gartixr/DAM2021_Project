14e50f2bc513e8ab4ec204a99dae3e333d7cec86
Gartixr <adidumitrica@gmail.com>
2021-05-12 18:20:13 +0200

    Modificar assets
    - Cambiar texturas
    - Añadir colisionador a las puertas
    - Aumentar distancia generacion +1

--- Assets/Scripts/CameraMovment.cs
+++ Assets/Scripts/CameraMovment.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class CameraMovment : MonoBehaviour
 {
+    public int position;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            if (position == 1)
+            {
+
+            } else if ()
+            {
+
+            } else if ()
+            {
+
+            } else if ()
+            {
+
+            }
+
             Debug.Log(other);
         }
     }
