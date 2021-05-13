e079eeca34d55464240f35a208795fc6d407d199
Gartixr <adidumitrica@gmail.com>
2021-05-13 20:49:34 +0200

    CameraMovment
    -Mover camera a la habitación donde se ecuentra el jugador

--- Assets/Scripts/CameraMovment.cs
+++ Assets/Scripts/CameraMovment.cs
@@ -4,26 +4,43 @@ using UnityEngine;
 
 public class CameraMovment : MonoBehaviour
 {
-    public int position;
+    public float speed = 100.0F;
+
+    private Vector3 startMarker;
+    private Vector3 endMarker;
+    private float startTime;
+    private float journeyLength;
+    private bool leaveRoom;
+
+    private void Start()
+    {
+        leaveRoom = true;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            if (position == 1)
-            {
-
-            } else if ()
-            {
-
-            } else if ()
-            {
-
-            } else if ()
-            {
-
-            }
-
-            Debug.Log(other);
+            leaveRoom = false;
+            startTime = Time.time;
+            startMarker = Camera.main.transform.position;
+            endMarker = new Vector3(transform.position.x, transform.position.y, -1);
+            journeyLength = Vector3.Distance(startMarker, endMarker);
         }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        leaveRoom = true;
+    }
+
+    private void Update()
+    {
+        if (!leaveRoom)
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            Camera.main.transform.position = Vector3.Lerp(startMarker, endMarker, fracJourney);
+        }
+
     }
 }
